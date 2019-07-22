d518737d7d6365ae76c0ff2e405881f99687903d
thbaskaran <thbaskaran@gmail.com>
2019-07-22 17:30:58 +0530

    Update Test1.js

--- Test1.js
+++ Test1.js
@@ -1,81 +1,112 @@
-class DeparmentHomeCarousel extends React.Component {
+class PopularViewAll extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {
-            departmentCarouselImages: []
+     super(props);
+       this.state = {
+           DepartmentspopularList: [],
+           currentPage: 1,
         }
+    }
+    handleClick = (event) => {
+        this.setState({
+            currentPage: Number(event.target.id)
+        });
+        $('#page-numbers li').css('background-color', 'black');
+        $(event.target).css('background-color', 'red')
     }
     componentDidMount() {
         var errorMessage = '';
         var appweburl = decodeURIComponent(getQueryStringParameter('SPAppWebUrl'));
         appweburl = appweburl.substring(0, appweburl.lastIndexOf('/'));
-        var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth() + 1; //January is 0
-        var yyyy = today.getFullYear();
-        if (dd < 10) {
-            dd = '0' + dd;
+        var id = getQueryStringParameter('ID');
+        if (id != "" && id != null) {
+            var subsitelinkQuery = constants.subsiteLinksQuery + id;
+            var subsitelinkUrl = appweburl + "/_api/web/lists/getbytitle('" + constants.subsiteLinkslistName + "')/items" + subsitelinkQuery
+            getServiceCall(subsitelinkUrl).then(data => {
+                this.setState({
+                    DepartmentspopularList: data.d.results
+                })
+                Communica.Part.init('PopularViewAll', 20);
+            }, error => {
+                errorMessage = error.responseJSON.error.message;
+                DepartmentLogerror('PopularViewAll', 'PopularViewAll', 'getServiceCall-OnpageLoad', errorMessage)
+            });
         }
-        if (mm < 10) {
-            mm = '0' + mm;
+        else {
+            var getpopularLinkUrl = appweburl + "/_api/web/lists/getbytitle('" + constants.PLibraryName + "')/items" + constants.popularLinkQuery
+            getServiceCall(getpopularLinkUrl).then(data => {
+                this.setState({
+                    DepartmentspopularList: data.d.results
+                })
+                Communica.Part.init('PopularViewAll', 20);
+            }, error => {
+                errorMessage = error.responseJSON.error.message;
+                DepartmentLogerror('PopularViewAll', 'PopularViewAll', 'getServiceCall-OnpageLoad', errorMessage)
+            });
         }
-        var today = yyyy + '-' + mm + '-' + dd;
-        today = today + 'T00%3a00%3a00';
-        var departmentquery = "'?$select=File,ImageDescription,ID,ImgUrl,ContentType0,ImageTitle,NavigateURLType,StartDate,ExpiryDate&$filter=((datetime'" + today + "' ge StartDate)and( datetime'" + today + "' le ExpiryDate)) and (IsActive eq 1)&$orderby=Modified desc &$expand=File'";
-        var getUrl = appweburl + "/_api/web/lists/GetByTitle('" + constants.departmentCarouselListName + "')/items" + departmentquery;
-        var departmentNameUrl = appweburl + "/_api/web/title"
-        getServiceCall(getUrl).then(data => {
-            this.setState({
-                departmentCarouselImages: data.d.results
-            })
-            sliderScript();
-        },error => {
-           errorMessage = error.responseJSON.error.message;
-            DepartmentLogerror('DeparmentHomeCarousel', 'CarouselImages', 'getServiceCall-OnpageLoad', errorMessage)
-        })
-        getServiceCall(departmentNameUrl).then(data => {
-            document.getElementById('departmentTile').innerHTML =data.d.Title;
-
-        },error => {
-            errorMessage = error.responseJSON.error.message;
-            DepartmentLogerror('DeparmentHomeCarousel', 'DepartmentName', 'getServiceCall-OnpageLoad', errorMessage)
-        })
-    }
-    imageRedirect = (imgUrl, RedirectionType, ID) => {
-        event.preventDefault();
-        if (RedirectionType == 'External') {
-            window.open(imgUrl, '_blank');
-        }
-        if (RedirectionType == 'Internal') {
-            window.open(DepReadMoreUrl + ID, '_parent');
-        }
+       
     }
     render() {
+        const { DepartmentspopularList, currentPage } = this.state;
+        const indexOfLastItem = currentPage * departmentPageCount;
+        const indexOfFirstItem = indexOfLastItem - departmentPageCount;
+        const renderItems = DepartmentspopularList.slice(indexOfFirstItem, indexOfLastItem).map((item, index) => {
+            return <li key={index}><a href={item.Popularlink} target="parent">{item.Title}</a></li>;
+        });
+
+        // Logic for displaying page numbers
+        const pageNumbers = [];
+        for (let i = 1; i <= Math.ceil(DepartmentspopularList.length / departmentPageCount); i++) {
+            pageNumbers.push(i);
+        }
+
+        const renderPageNumbers = pageNumbers.map(number => {
+            return (
+                <li
+                    key={number}
+                    id={number}
+                    onClick={this.handleClick}>{number}
+                </li>
+            );
+        });
         return (
-            <div className="demo">
-                <ul id="lightSlider" className="light">
-                    { this.state.departmentCarouselImages.map((individualbannerimage) =>
-                        <li key={individualbannerimage.ID} data-src={individualbannerimage.ContentType0} data-thumb={individualbannerimage.ContentType0 == 'Image' ? individualbannerimage.File.ServerRelativeUrl : (individualbannerimage.ContentType0 == 'Video' ? individualbannerimage.File.ServerRelativeUrl : (individualbannerimage.ContentType0 == 'Text' ? individualbannerimage.ImageTitle : null))}>
-                            {individualbannerimage.ContentType0 == 'Image'
-                                ? <img onClick={() => this.imageRedirect(individualbannerimage.ImageUrl, individualbannerimage.NavigateURLType, individualbannerimage.ID)} src={individualbannerimage.File.ServerRelativeUrl} />
-                                : (individualbannerimage.ContentType0 == 'Video'
-                                    ? <video width="100%" height="350px" controls src={individualbannerimage.File.ServerRelativeUrl}>
-                                           Your browser does not support HTML5 video.</video>
-                                    : (individualbannerimage.ContentType0 == 'Text'
-                                        ? <p className="bannertext-mobileview" dangerouslySetInnerHTML={{ __html: individualbannerimage.ImageDescription }}></p>
-                                        : null
-                                    )
-                                )
-                            } 
-                             <div className="banner-parentdiv">
-                                <p className="banner-childtext">{individualbannerimage.ImageTitle} <span><a onClick={() => this.imageRedirect(individualbannerimage.ImageUrl, individualbannerimage.NavigateURLType, individualbannerimage.ID)} className="banner_readmore">Read More</a></span></p>
-                              </div>
-                         </li>
-                    )}
-                  </ul>
-               </div>
-             
-             )
-         }
-     }
-ReactDOM.render(<DeparmentHomeCarousel />, document.getElementById('DepartmentHomeCarousel'));
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 col-xs-12">
+                            <div className="row">
+                                <div className="col-md-2 col-xs-12"></div>
+                                <div className="col-md-8 col-xs-12">
+                                        <div className="seeall-welledit">
+                                            <p className="title-header">Popular Links</p>
+                                        <p><label className="popular_redline"></label></p>
+                                        {this.state.DepartmentspopularList && (this.state.DepartmentspopularList != 0) ?
+                                            <div>
+                                                <div className="table-responsive top1 ">
+                                                    <table className="table fulltable ">
+                                                        <tbody className="tbl-body">
+                                                            <tr>
+                                                                <td>{renderItems}</td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <div className="top2 clearfix">
+                                                    <div className="pull-right">
+                                                        <ul id="page-numbers">{renderPageNumbers}</ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            : <label className="lbl-text">There are no list to display</label>
+                                        }
+                                        </div>
+                                </div>
+                                <div className="col-md-2 col-xs-12"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+ReactDOM.render(<PopularViewAll />, document.getElementById('PopularViewAll'));
